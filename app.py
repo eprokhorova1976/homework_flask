1f2aef8c9091771ae5f21952a3afd6d7a636259c
eprokhorova1976 <154540454+eprokhorova1976@users.noreply.github.com>
2024-06-10 21:03:16 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ def hello():
     return 'Welcome to online excursions shop. Choose what you are interested in.'
 
 
-@app.route('/excursions/')
-def excursions():
-    return render_template('excursions.html')
+# @app.route('/excursions/')
+# def excursions():
+#     return render_template('excursions.html')
 
 
 @app.route('/daily_diving/')
@@ -18,17 +18,17 @@ def daily_diving():
     return render_template('daily.html')
 
 
-@app.route('/exc/')
-def exc():
-    exc= [
-        {"title":"Daily diving", "price":"45$ p.p.",
+@app.route('/excursions/')
+def excursions():
+    excursions= [
+        {"title":"Daily diving", "price":"45$ p.p.", "img":"/static/images/2.png", "address":"/daily/",
          "description":"Full day trip. 2 diving, transfer, equipment, lunch, soft drinks"},
-        {"title": "Wreck diving. Salem Express", "price": "150$ p.p.",
+        {"title": "Wreck diving. Salem Express", "price": "150$ p.p.", "img":"/static/images/3.png", "address":"#",
          "description": "Full day trip. 2 diving (wreck, coral reef), transfer, equipment, lunch, soft drinks"},
-        {"title": "Night diving", "price": "60$ p.p.",
+        {"title": "Night diving", "price": "60$ p.p.", "img":"/static/images/4.png", "address":"#",
          "description": "Full day trip. 1 diving, transfer, equipment"}
     ]
-    return render_template('excursions.html', exc=exc)
+    return render_template('excursions.html', excursions=excursions)
 
 
 if __name__=='__main__':
