f25d006bb99b9d3f113b041d794b736482146455
eprokhorova1976 <154540454+eprokhorova1976@users.noreply.github.com>
2024-06-10 17:29:37 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Welcome to online excursions shop. Choose what you are interested in.'
+
+
+@app.route('/index/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/excursions/')
+def excursions():
+    return render_template('excursions.html')
+
+
+@app.route('/diving/')
+def diving():
+    return render_template('diving.html')
+
+
+@app.route('/daily_diving/')
+def daily_diving():
+    return render_template('daily.html')
+
+
+@app.route('/exc/')
+def exc():
+    exc= [
+        {"title":"Daily diving", "price":"45$ p.p.",
+         "description":"Full day trip. 2 diving, transfer, equipment, lunch, soft drinks"},
+        {"title": "Wreck diving. Salem Express", "price": "150$ p.p.",
+         "description": "Full day trip. 2 diving (wreck, coral reef), transfer, equipment, lunch, soft drinks"},
+        {"title": "Night diving", "price": "60$ p.p.",
+         "description": "Full day trip. 1 diving, transfer, equipment"}
+    ]
+    return render_template('excursions.html', exc=exc)
+
+
+if __name__=='__main__':
+    app.run()
