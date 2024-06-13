67ae205d0ebd0f06afcef9d256dabd0eddf6c99a
eprokhorova1976 <154540454+eprokhorova1976@users.noreply.github.com>
2024-06-13 15:20:03 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
-
+app.secret_key='e98ecfcbcac9988e748e82151c28086b8dc77da939a829bd0a15ef4b75a265d7'
 
 @app.route('/')
-def hello():
-    return 'Welcome to online excursions shop. Choose what you are interested in.'
-
-
-# @app.route('/excursions/')
-# def excursions():
-#     return render_template('excursions.html')
+def index():
+    return render_template('index.html')
 
 
 @app.route('/daily_diving/')
@@ -38,6 +33,24 @@ def daily():
          "description":"Full day trip. Starts from 08.00 a.m. and finished at 03.30 p.m. 2 diving on different coral reefs. Transfer from hotel and back, diving equipment, lunch on the boat, soft drinks during trip are included"}
             ]
     return render_template('daily.html', daily=daily)
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form.get('name', 'email')
+        return redirect(url_for('index'))
+    return render_template('login.html')
+
+
+@app.route('/logout/')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
+
+
 
 
 if __name__=='__main__':
